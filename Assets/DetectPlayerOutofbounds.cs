2e0a683b0331e4e361585149edbf7e24542223a8
alxyzz <93402865+alxyzz@users.noreply.github.com>
2023-09-19 15:51:23 +0200

    out of bounds teleportation

---
+++ Assets/DetectPlayerOutofbounds.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectPlayerOutofbounds : MonoBehaviour
+{
+
+    public GameObject teleportLocation; //where player gets moved if they touch this object
+    public CharacterController controller;
+    void OnTriggerEnter(Collider colli)
+    {
+        if (colli.tag == "Player")
+        {
+            controller.enabled = false;
+
+            controller.transform.position = teleportLocation.transform.position;
+
+            controller.enabled = true;
+        }
+    }
+}
