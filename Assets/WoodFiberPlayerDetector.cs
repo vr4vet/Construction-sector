a940742fd54624613defd188737f5c93725aa32b
alxyzz <93402865+alxyzz@users.noreply.github.com>
2023-12-07 21:50:19 +0100

    some tweaks for materials

--- Assets/WoodFiberPlayerDetector.cs
+++ Assets/WoodFiberPlayerDetector.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class WoodFiberPlayerDetector : MonoBehaviour
 {
     [SerializeReference]WoodFibreAdjustment adj;
+     Renderer _rend;
     GameObject firsthandin; //because there are a lot of things called Player, and we don't want to disable the player's ability to interact just because their other hand went out of the radius
     private void OnTriggerEnter(Collider other)
     {
@@ -15,7 +16,13 @@ public class WoodFiberPlayerDetector : MonoBehaviour
             
         }
     }
-
+    void Start()
+    {
+        if (_rend == null)
+        {
+            _rend = GetComponent<Renderer>();
+        }
+    }
 
     private void OnTriggerExit(Collider other)
     {
@@ -23,6 +30,7 @@ public class WoodFiberPlayerDetector : MonoBehaviour
         {
             firsthandin = null;
             adj.PlayerLeft();
+            _rend.enabled = false;
         }
     }
 }
