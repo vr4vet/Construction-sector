571e5085b058609d55222b60fd6784522bfdef5b
alxyzz <93402865+alxyzz@users.noreply.github.com>
2024-06-30 21:10:46 +0200

    tablet fully functional

--- Assets/ConstructionScenario/Scripts/ConstructionManager.cs
+++ Assets/ConstructionScenario/Scripts/ConstructionManager.cs
@@ -51,37 +51,43 @@ public class ConstructionManager : MonoBehaviour
             _subtask = SubTaskEnum.ZERO;
             SwitchElementVisibility(subtaskObjects[0]);
 			 _narrator.Narrate("Task 1, Subtask 0 - Safety equipment!<br>Pick up the correct protective equipment.");
+            CompleteElement(SubTaskEnum.START);
         }
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             _subtask = SubTaskEnum.ONE;
             SwitchElementVisibility(subtaskObjects[1]);
 			_narrator.Narrate("Task 1, Subtask 1 - Create a wooden frame wall \n Pick up a beam from the table and attach it to the corresponding spot at one of the green outlines.");
+            CompleteElement(SubTaskEnum.ZERO);
         }
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             _subtask = SubTaskEnum.TWO;
             SwitchElementVisibility(subtaskObjects[2]);
 			 _narrator.Narrate("Task 1, Subtask 2 - Attach the housewrap for the outer layer, then staple it. Tape the holes, if any.");
+            CompleteElement(SubTaskEnum.ONE);//always one less because we dont want the task to be completed...
         }
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             _subtask = SubTaskEnum.THREE;
             SwitchElementVisibility(subtaskObjects[3]);
 			 _narrator.Narrate("Task 1, Subtask 3 - Insert wood fiber insulation into the frame, then arrange it neatly into place.");
+            CompleteElement(SubTaskEnum.TWO);//always one less because we dont want the task to be completed...
         }
         if (Input.GetKeyDown(KeyCode.Alpha4))
         {
             _subtask = SubTaskEnum.FOUR;
             SwitchElementVisibility(subtaskObjects[4]);
 			_narrator.Narrate("Task 1, Subtask 4 - Apply the vapor foil, stapling and taping it.");
-                
+            CompleteElement(SubTaskEnum.THREE);//always one less because we dont want the task to be completed...
+
         }
         if (Input.GetKeyDown(KeyCode.Alpha5))
         {
             _subtask = SubTaskEnum.FIVE;
             SwitchElementVisibility(subtaskObjects[5]);
 			_narrator.Narrate("Task 2, Subtask 1 - Learn the layers of a roof, then fill out a short quiz.");
+            CompleteElement(SubTaskEnum.FOUR);//always one less because we dont want the task to be completed...
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha6))
@@ -90,12 +96,23 @@ public class ConstructionManager : MonoBehaviour
 
             SwitchElementVisibility(subtaskObjects[6]);
 			_narrator.Narrate("Task 2, Subtask 2 - Assemble the tiling of a roof.");
+            CompleteElement(SubTaskEnum.FIVE);//always one less because we dont want the task to be completed...
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha7))
+        {
+            _subtask = SubTaskEnum.SEVEN;
+
+            SwitchElementVisibility(subtaskObjects[6]);
+ 
+            CompleteElement(SubTaskEnum.SIX);//always one less because we dont want the task to be completed...
+
         }
 
     }
 
     public enum SubTaskEnum
     {
+        START,
         ZERO,
         ONE,
         TWO,
@@ -135,38 +152,93 @@ public class ConstructionManager : MonoBehaviour
     public Task.Task T1, T2;
 
     [Header("Subtasks")]
-    public Subtask T1_S0_protection_equipment, T1_S1, T1_S2, T1_S3, T1_S4, T2_S1, T2_S2;
-    void CompleteElement(SubTaskEnum which)
+    public Task.Subtask T1_S0_protection_equipment, T1_S1, T1_S2, T1_S3, T1_S4, T2_S1, T2_S2;
+
+    List<Task.Subtask> subtasks;
+    public void CompleteElement(SubTaskEnum which)
     {
-
-
+        subtasks = new List<Task.Subtask>() { T1_S0_protection_equipment, T1_S1, T1_S2, T1_S3, T1_S4, T2_S1, T2_S2 };
         switch (which)
         {
+            case SubTaskEnum.START:
+
+                T1_S0_protection_equipment.SetCompleated(false);
+                T1_S1.SetCompleated(false);
+                T1_S2.SetCompleated(false);
+                T1_S3.SetCompleated(false);
+                T1_S4.SetCompleated(false);
+                T2_S1.SetCompleated(false);
+                T2_S2.SetCompleated(false);
+                break;
             case SubTaskEnum.ZERO:
                 T1_S0_protection_equipment.SetCompleated(true);
+                T1_S1.SetCompleated(false);
+                T1_S2.SetCompleated(false);
+                T1_S3.SetCompleated(false);
+                T1_S4.SetCompleated(false);
+                T2_S1.SetCompleated(false);
+                T2_S2.SetCompleated(false);
                 break;
             case SubTaskEnum.ONE:
+                T1_S0_protection_equipment.SetCompleated(true);
                 T1_S1.SetCompleated(true);
-
+                T1_S2.SetCompleated(false);
+                T1_S3.SetCompleated(false);
+                T1_S4.SetCompleated(false);
+                T2_S1.SetCompleated(false);
+                T2_S2.SetCompleated(false);
                 break;
             case SubTaskEnum.TWO:
+                T1_S0_protection_equipment.SetCompleated(true);
                 T1_S1.SetCompleated(true);
+                T1_S2.SetCompleated(true);
+                T1_S3.SetCompleated(false);
+                T1_S4.SetCompleated(false);
+                T2_S1.SetCompleated(false);
+                T2_S2.SetCompleated(false);
                 break;
             case SubTaskEnum.THREE:
+                T1_S0_protection_equipment.SetCompleated(true);
                 T1_S1.SetCompleated(true);
+                T1_S2.SetCompleated(true);
+                T1_S3.SetCompleated(true);
+                T1_S4.SetCompleated(false);
+                T2_S1.SetCompleated(false);
+                T2_S2.SetCompleated(false);
                 break;
             case SubTaskEnum.FOUR:
+                T1_S0_protection_equipment.SetCompleated(true);
                 T1_S1.SetCompleated(true);
+                T1_S2.SetCompleated(true);
+                T1_S3.SetCompleated(true);
+                T1_S4.SetCompleated(true);
+                T2_S1.SetCompleated(false);
+                T2_S2.SetCompleated(false);
+                T1.Compleated(true);
+                T1.Compleated(true);
                 break;
             case SubTaskEnum.FIVE:
+                T1_S0_protection_equipment.SetCompleated(true);
                 T1_S1.SetCompleated(true);
+                T1_S2.SetCompleated(true);
+                T1_S3.SetCompleated(true);
+                T1_S4.SetCompleated(true);
+                T2_S1.SetCompleated(true);
+                T2_S2.SetCompleated(false);
+                T1.Compleated(true);
                 break;
             case SubTaskEnum.SIX:
+                T1_S0_protection_equipment.SetCompleated(true);
                 T1_S1.SetCompleated(true);
+                T1_S2.SetCompleated(true);
+                T1_S3.SetCompleated(true);
+                T1_S4.SetCompleated(true);
+                T2_S1.SetCompleated(true);
+                T2_S2.SetCompleated(true);
+                T1.Compleated(true);
+                T2.Compleated(true);
                 break;
             case SubTaskEnum.SEVEN:
-                T2_S2.SetCompleated(true);
-                T2.Compleated(true);
                 break;
             default:
                 break;
@@ -186,39 +258,53 @@ public void HasFinishedSubtask(SubTaskEnum stask)
         switch (_subtask)
         {
             case SubTaskEnum.ZERO:
+                CompleteElement(SubTaskEnum.START);//prepares the grounds
                 SwitchElementVisibility(subtaskObjects[0]);
                 _narrator.Narrate("Task 1, Subtask 0 - Safety equipment!<br>Pick up the correct protective equipment.");
                 break;
             case SubTaskEnum.ONE:
+                CompleteElement(SubTaskEnum.ZERO);//completes the last task
+
                 SwitchElementVisibility(subtaskObjects[1]);
                 _narrator.Narrate("Task 1, Subtask 1 - Create a wooden frame wall \n Pick up a beam from the table and attach it to the corresponding spot at one of the green outlines.");
                 break;
             case SubTaskEnum.TWO:
+                CompleteElement(SubTaskEnum.ONE);
+
                 SwitchElementVisibility(subtaskObjects[2]);
                 _narrator.Narrate("Task 1, Subtask 2 - Attach the housewrap for the outer layer, then staple it. Tape the holes, if any.");
                 break;
             case SubTaskEnum.THREE:
+                CompleteElement(SubTaskEnum.TWO);
+
                 SwitchElementVisibility(subtaskObjects[3]);
                 _narrator.Narrate("Task 1, Subtask 3 - Insert wood fiber insulation into the frame, then arrange it neatly into place.");
                 break;
             case SubTaskEnum.FOUR:
+                CompleteElement(SubTaskEnum.THREE);
+
                 SwitchElementVisibility(subtaskObjects[4]);
                 _narrator.Narrate("Task 1, Subtask 4 - Apply the vapor foil, stapling and taping it.");
                 break;
 
             case SubTaskEnum.FIVE:
+                CompleteElement(SubTaskEnum.FOUR);
+
                 SwitchElementVisibility(subtaskObjects[5]);
                 _narrator.Narrate("Task 2, Subtask 1 - Learn the layers of a roof, then fill out a short quiz.");
                 StartCoroutine(narrateT2S1());
                 break;
 
             case SubTaskEnum.SIX:
+                CompleteElement(SubTaskEnum.FIVE);
+
                 SwitchElementVisibility(subtaskObjects[6]);
                 _narrator.Narrate("Task 2, Subtask 2 - Assemble the tiling of a roof.");
                 break;
 
             case SubTaskEnum.SEVEN:
-                
+                CompleteElement(SubTaskEnum.SIX);
+
                 break;
             default:
                 break;
